f0620613e8a61e051a6be1aaa1bb3aa33127fbfa
Yurii Soldak <ysoldak@gmail.com>
2025-06-03 21:36:17 +0200

    Support MSP AU on HW25
    Also:
    - Fixes ports configuration on HW26
    - Adds Settings address constants

--- src/page_device.go
+++ src/page_device.go
@@ -64,51 +64,51 @@ func (pd *PageDevice) Enter() {
 func (pd *PageDevice) ItemsFromSettings() []Item {
 	items := []Item{}
 	items = append(items, NewItemSimple("- BATTLE ---------"))
-	items = append(items, NewItemByte("Team", 72, 0x0A, 0x0E, 1).WithDrawer(&TeamNameDrawer{}))
-	items = append(items, NewItemByte("Player", 73, 1, 9, 1))
-	items = append(items, NewItemByte("Life", 70, 1, 255, 1))
-	items = append(items, NewItemByte("Ammo", 71, 1, 255, 1))
-	items = append(items, NewItemByte("Shoot Power", 75, 1, 10-settings.Get(76)-settings.Get(77), 1))
-	items = append(items, NewItemByte("Shoot Rate", 76, 1, 10-settings.Get(75)-settings.Get(77), 1))
-	items = append(items, NewItemByte("Armor", 77, 1, 10-settings.Get(75)-settings.Get(76), 1))
+	items = append(items, NewItemByte("Team", S_TEAM, 0x0A, 0x0E, 1).WithDrawer(&TeamNameDrawer{}))
+	items = append(items, NewItemByte("Player", S_PLAYER, 1, 9, 1))
+	items = append(items, NewItemByte("Life", S_LIFE, 1, 255, 1))
+	items = append(items, NewItemByte("Ammo", S_AMMO, 1, 255, 1))
+	items = append(items, NewItemByte("Shoot Power", S_SHOOT_PWR, 1, 10-settings.Get(76)-settings.Get(77), 1))
+	items = append(items, NewItemByte("Shoot Rate", S_SHOOT_RATE, 1, 10-settings.Get(75)-settings.Get(77), 1))
+	items = append(items, NewItemByte("Armor", S_SHOOT_ARMOR, 1, 10-settings.Get(75)-settings.Get(76), 1))
 	items = append(items, NewItemSimple("- DISPLAY --------"))
-	items = append(items, NewItemString("Name", 100, 10))
-	items = append(items, NewItemByte("Canvas", 95, 0, 4, 1).WithDrawer(NewNamesDrawer("30x16", "50x18", "30x16C", "60x22", "53x20")).WithValuer(&BitsValuer{0b00011100}))
-	items = append(items, NewItemByte("Icons", 95, 0, 2, 1).WithDrawer(NewNamesDrawer("Off", "Short", "Full")).WithValuer(&BitsValuer{0b00000011}))
-	items = append(items, NewItemByte("Debug", 96, 0, 1, 1).WithDrawer(NewNamesDrawer("Off", "On")).WithValuer(&BitsValuer{0b00000011}))
+	items = append(items, NewItemString("Name", S_NAME, 10))
+	items = append(items, NewItemByte("Canvas", S_DIGITAL, 0, 4, 1).WithDrawer(NewNamesDrawer("30x16", "50x18", "30x16C", "60x22", "53x20")).WithValuer(&BitsValuer{0b00011100}))
+	items = append(items, NewItemByte("Icons", S_DIGITAL, 0, 2, 1).WithDrawer(NewNamesDrawer("Off", "Short", "Full")).WithValuer(&BitsValuer{0b00000011}))
+	items = append(items, NewItemByte("Debug", S_DEBUG, 0, 1, 1).WithDrawer(NewNamesDrawer("Off", "On")).WithValuer(&BitsValuer{0b00000011}))
 	items = append(items, NewItemSimple("- EFFECTS --------"))
-	items = append(items, NewItemByte("Alive", 80, 0, 1, 1).WithDrawer(NewNamesDrawer("Pass", "Custom")))
-	if settings.Get(80) == 1 { // Custom
-		items = append(items, NewItemByte(" Custom", 81, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
+	items = append(items, NewItemByte("Alive", S_E_ALIVE, 0, 1, 1).WithDrawer(NewNamesDrawer("Pass", "Custom")))
+	if settings.Get(S_E_ALIVE) == 1 { // Custom
+		items = append(items, NewItemByte(" Custom", S_E_ALIVE_CUSTOM, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
 	}
-	items = append(items, NewItemByte("Dead", 82, 0, 1, 1).WithDrawer(NewNamesDrawer("Pass", "Custom")))
-	if settings.Get(82) == 1 { // Custom
-		items = append(items, NewItemByte(" Custom", 83, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
+	items = append(items, NewItemByte("Dead", S_E_DEAD, 0, 1, 1).WithDrawer(NewNamesDrawer("Pass", "Custom")))
+	if settings.Get(S_E_DEAD) == 1 { // Custom
+		items = append(items, NewItemByte(" Custom", S_E_DEAD_CUSTOM, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
 	}
-	items = append(items, NewItemByte("Hit", 84, 0, 2, 1).WithDrawer(NewNamesDrawer("Pass", "Custom", "Shake")))
-	if settings.Get(84) != 0 { // !Pass
-		items = append(items, NewItemByte(" Duration MS", 85, 1, 120, 1).WithDrawer(&LinearDrawer{200, 0}))
+	items = append(items, NewItemByte("Hit", S_E_HIT, 0, 2, 1).WithDrawer(NewNamesDrawer("Pass", "Custom", "Shake")))
+	if settings.Get(S_E_HIT) != 0 { // !Pass
+		items = append(items, NewItemByte(" Duration MS", S_E_HIT_DURATION, 1, 120, 1).WithDrawer(&LinearDrawer{200, 0}))
 	}
-	if settings.Get(84) == 1 { // Custom
-		items = append(items, NewItemByte(" Custom", 86, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
+	if settings.Get(S_E_HIT) == 1 { // Custom
+		items = append(items, NewItemByte(" Custom", S_E_HIT_CUSTOM, 0, 24, 1).WithDrawer(&LinearDrawer{50, 900}))
 	}
-	if settings.Get(84) == 2 { // Shake
-		items = append(items, NewItemByte(" Shake Every", 87, 1, 10, 1).WithDrawer(&LinearDrawer{200, 0}))
-		items = append(items, NewItemByte(" Shake Level", 88, 1, 12, 1).WithDrawer(&LinearDrawer{50, 0}))
+	if settings.Get(S_E_HIT) == 2 { // Shake
+		items = append(items, NewItemByte(" Shake Every", S_E_HIT_SHAKE_EVERY, 1, 10, 1).WithDrawer(&LinearDrawer{200, 0}))
+		items = append(items, NewItemByte(" Shake Level", S_E_HIT_SHAKE_LEVEL, 1, 12, 1).WithDrawer(&LinearDrawer{50, 0}))
 	}
 	items = append(items, NewItemSimple("- HARDWARE -------"))
-	items = append(items, NewItemByte("Team Leds", 91, 0, 255, 1))
-	items = append(items, NewItemByte("Info Leds", 92, 0, 50, 2))
-	items = append(items, NewItemByte("Voltage", 93, 1, 255, 1))
+	items = append(items, NewItemByte("Team Leds", S_TEAM_LED, 0, 255, 1))
+	items = append(items, NewItemByte("Info Leds", S_INFO_LED, 0, 50, 2))
+	items = append(items, NewItemByte("Voltage", S_VOLTAGE, 1, 255, 1))
 
+	if strings.Contains(pd.device.Hardware, "2.5") {
+		items = append(items, NewItemByte("HC12 Pins", S_PORTS, 0, 2, 1).WithDrawer(NewNamesDrawer("OFF", "CSP", "MSP AU")).WithValuer(&BitsValuer{0b00000011}))
+		items = append(items, NewItemByte("I2C  Pins", S_PORTS, 0, 1, 1).WithDrawer(NewNamesDrawer("I2C", "CSP")).WithValuer(&BitsValuer{0b00110000}))
+	}
 	if strings.Contains(pd.device.Hardware, "2.6") {
-		if strings.HasPrefix(pd.device.Firmware, "2.7") {
-			items = append(items, NewItemByte("HC12 Pins", 94, 0, 3, 1).WithDrawer(NewNamesDrawer("Off", "CSP", "MSP AU", "MSP FC")).WithValuer(&BitsValuer{0b00000011}))
-			items = append(items, NewItemByte("I2C  Pins", 94, 0, 1, 1).WithDrawer(NewNamesDrawer("I2C", "CSP")).WithValuer(&BitsValuer{0b00110000}))
-			items = append(items, NewItemByte("MSP  Pins", 94, 0, 3, 1).WithDrawer(NewNamesDrawer("Auto", "Off", "MSP AU", "MSP FC")).WithValuer(&BitsValuer{0b00001100}))
-		} else {
-			items = append(items, NewItemByte("MSP  Pins", 94, 0, 2, 1).WithDrawer(NewNamesDrawer("Auto", "Off", "MSP AU")).WithValuer(&BitsValuer{0b00001100}))
-		}
+		items = append(items, NewItemByte("HC12 Pins", S_PORTS, 0, 3, 1).WithDrawer(NewNamesDrawer("OFF", "CSP", "MSP AU", "MSP FC")).WithValuer(&BitsValuer{0b00000011}))
+		items = append(items, NewItemByte("I2C  Pins", S_PORTS, 0, 1, 1).WithDrawer(NewNamesDrawer("I2C", "CSP")).WithValuer(&BitsValuer{0b00110000}))
+		items = append(items, NewItemByte("MSP  Pins", S_PORTS, 0, 3, 1).WithDrawer(NewNamesDrawer("AUTO", "OFF", "MSP AU", "MSP FC")).WithValuer(&BitsValuer{0b00001100}))
 	}
 	return items
 }
